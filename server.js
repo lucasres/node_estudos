810889ee22d665ca10e4aaa2e275b541bb581fdc
ubuntu <lucasresone@gmail.com>
2019-05-25 14:15:52 -0700

    add mongoose in docker-compose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
+//config db
+db = mongoose.connect('mongoose://root:root@mongo:27017/');
+
 app.get('/',function(req,res){
-    res.send('Lorem');
+    res.send('First Node Server heheh');
 });
 
 app.listen('3000',function(){
