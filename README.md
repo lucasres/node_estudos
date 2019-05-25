d5cff53b59dc1b8dc6c233c7e435acf9eeae8db1
ubuntu <lucasresone@gmail.com>
2019-05-25 15:35:13 -0700

    model Product Ok

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ You deploy the app with docker. Use this command:
 ```
 docker-compose up -d --build
 ```
+
+# Dependecy
+
+Express: Router
+Mongoose: ORM
+Require-dir: Import all modules of dir 
--- server.js
+++ server.js
@@ -4,10 +4,27 @@ var mongoose = require('mongoose');
 var app = express();
 
 //config db
-db = mongoose.connect('mongoose://root:root@mongo:27017/');
+console.log('inicio conecao');
+try{
+    db = mongoose.connect('mongodb://mongo:27017/nodeapi',{ useNewUrlParser: true });
+} catch(e) {
+    console.log('deu erro na conecao com o mongodb');
+    console.log(e);
+}
+//registra o model
+require('./src/models/Product');
+
+const Product = mongoose.model('Product');
 
 app.get('/',function(req,res){
-    res.send('First Node Server heheh');
+    Product.create({
+        title:'a',
+        description:'b',
+        url:'c'
+    });
+    console.log('criou');
+    
+    return res.send('First Nod e Server heheh');
 });
 
 app.listen('3000',function(){
