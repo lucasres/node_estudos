8088c8408ab60d86e3a0b09daa724f47ac971a15
mandala21 <lucasresone@gmail.com>
2019-06-01 12:15:32 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var app = express();
 //permite que a aplicacao user json
 app.use(express.json());
+//add cors para que outros dominios possam acessar nossa api
+app.use(cors());
 //config db
 console.log('inicio conecao com o mongodb');
 try{
